9b372008e74952b9eab75a674892556048284ebb
Aranch <aranzh@yonsei.ac.kr>
2020-01-26 15:10:33 +0900

    Divide Conquer:  square, Fermat

--- 2019_W/DivideConquer/11401_BinaryCo3.cpp
+++ 2019_W/DivideConquer/11401_BinaryCo3.cpp
@@ -9,11 +9,49 @@ void init(){
     cin>>n>>k;
 }
 
-long long nck(int a,int b){
-    if(a==b) return 1;
+
+long long power(long long a,long long b){
+    //cout<<b<<endl;
     if(b==0) return 1;
+    if(b==1) return a%R;
     else{
-        return nck(a-1,b-1)%R+nck(a-1,b)%R;
-        
+        if(b%2==0){
+            long long half=power(a,b/2)%R;
+            return (half*half)%R;
+        }
+        else{
+            return (a*power(a,b-1))%R;
+        }
     }
+}
+
+
+long long nck(){
+    long long nFac=1;
+    for(long long i=1;i<=n;i++){
+        nFac*=i;
+        nFac=nFac%R;
+    }
+    //cout<<"N\n";
+    long long kFac=1;
+    for(long long i=1;i<=n-k;i++){
+        kFac*=i;
+        kFac=kFac%R;
+    }
+    //cout<<"n-k\n";
+    for(long long i=1;i<=k;i++){
+        kFac*=i;
+        kFac=kFac%R;
+    }
+    //cout<<"K\n";
+    long long under=kFac%R;
+    //cout<<"under\n";
+    return ((nFac%R)*(power(under,R-2)%R))%R;
+}
+
+int main(){
+    init();
+    cout<<nck();
+    //cout<<"end";
+    return 0;
 }
