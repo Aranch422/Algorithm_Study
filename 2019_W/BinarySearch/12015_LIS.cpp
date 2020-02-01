8b7529e6149a6ed285097529c1ed9911ebb2d2a7
Aranch <aranzh@yonsei.ac.kr>
2020-02-02 02:33:57 +0900

    LIS with BS

--- 2019_W/BinarySearch/12015_LIS.cpp
+++ 2019_W/BinarySearch/12015_LIS.cpp
@@ -1,40 +1,51 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int n;
 int num[1000000];
-int dp[1000000];
+vector<int> Lis;
 
 void init(){
     cin>>n;
     for(int i=0;i<n;i++){
         cin>>num[i];
     }
-    dp[0]=1;
-    int left,right;
-    for(int i=1;i<n;i++){
-        left=0; right=i-1;
-        int dpmax=0;
-        while(left!=right){
-            int mid=(left+right)/2;
-            if(dpmax<=dp[mid]){
-                if(dp[mid]<dp[i]){
-                    dpmax=dp[mid];
-                }
-                
-            }
-            else{
-                if(dp[mid]<dp[i]);
-            }
+}
+
+void solve(){
+    for(int i=0;i<n;i++){
+        if(Lis.size()==0){
+            Lis.push_back(num[i]);
         }
-        //left==right 해주고
-        
-        dp[i]=dpmax;
+        else if(Lis.back()<num[i]){
+            Lis.push_back(num[i]);
+        }
+        else{
+            int left=0;
+            int right=Lis.size()-1;
+            while(left<right){
+                int mid=(left+right)/2;
+                if(Lis[mid]<num[i]){
+                    left=mid+1;
+                }
+                else right=mid;
+            }
+            Lis[left]=num[i];
+        }
+        /*
+        for(int i=0;i<Lis.size();i++){
+            cout<<Lis[i]<<" ";
+        }
+        cout<<endl;
+        */
     }
+    cout<<Lis.size();
 }
 
 int main(){
-
+    init();
+    solve();
     return 0;
 }
