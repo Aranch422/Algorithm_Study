088dcb6379f08b61153e816f1f5d9f1f29d925f2
Aranch <aranzh@yonsei.ac.kr>
2020-02-20 14:27:02 +0900

    While

---
+++ 2019_W/Queue/7662_dualPriorityQ.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+void solve(){
+    int k;
+    cin>>k;
+    for(int i=0;i<k;i++){
+        string s;
+        cin>>s;
+        if(s=="I"){
+            int num;
+            cin>>num;
+            
+        }
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+
+    }
+    return 0;
+}
