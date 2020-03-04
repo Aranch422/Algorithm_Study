f515393d1b66e9d8cbd0b4209d338c28a13089fc
Aranch <aranzh@yonsei.ac.kr>
2020-03-05 02:27:30 +0900

    Dual Priority Queue: check like bit masking

--- 2019_W/Queue/7662_dualPriorityQ2.cpp
+++ 2019_W/Queue/7662_dualPriorityQ2.cpp
@@ -1,9 +1,77 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 
 using namespace std;
 
-int main(){
 
+void solve(){
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> Min;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,less<pair<int,int>>> Max;
+    
+    int idx=0;
+    vector<bool> isDeleted;
+    int n;
+    cin>>n;
+    while(n>0){
+        n--;
+        string s;
+        cin>>s;
+        if(s=="I"){
+            int num;
+            cin>>num;
+            Max.push(make_pair(num,idx));
+            Min.push(make_pair(num,idx));
+            idx++;
+            isDeleted.push_back(false);
+        }
+        if(s=="D"){
+            int num;
+            cin>>num;
+            if(Max.empty()||Min.empty()) continue;
+            //del Max
+            if(num==1){
+                while(!Max.empty()&&isDeleted[Max.top().second]){
+                    Max.pop();
+                }
+                if(!Max.empty()){
+                    isDeleted[Max.top().second]=true;
+                    Max.pop();
+                }
+            }
+
+            if(num==-1){
+                while(!Min.empty()&&isDeleted[Min.top().second]){
+                    Min.pop();
+                }
+                if(!Min.empty()){
+                    isDeleted[Min.top().second]=true;
+                    Min.pop();
+                }
+            }
+        }
+    }
+    while(!Max.empty()&&isDeleted[Max.top().second]){
+        Max.pop();
+    }
+    while(!Min.empty()&&isDeleted[Min.top().second]){
+        Min.pop();
+    }
+    if(Max.empty()||Min.empty()) cout<<"EMPTY\n";
+    else{
+        cout<<Max.top().first<<" "<<Min.top().first<<'\n';
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int k;
+    cin>>k;
+    while(k>0){
+        k--;
+        solve();
+    }
     return 0;
 }
