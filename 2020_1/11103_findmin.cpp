2089a86a73c854b2db5f3c05316020c885b7182b
Aranch <aranzh@yonsei.ac.kr>
2020-06-29 15:26:11 +0900

    Summer Start

---
+++ 2020_1/11103_findmin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int num[5000001];
+
+int res=-98765432110;
+
+int n,L;
+void init(){
+    cin>>n>>L;
+    for(int i=1;i<=n;i++){
+        cin>>num[i];
+        if(i<=L){
+            if(num[i]<min)
+        }
+    } 
+}
+
+int main(){
+
+    return 0;
+}
