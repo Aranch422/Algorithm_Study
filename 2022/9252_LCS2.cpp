37cf188f9247f2b9d916d9ca688d78e16295548c
Aranch422 <aranzh@yonsei.ac.kr>
2022-04-11 14:17:49 +0000

    LCS2 review need

--- 2022/9252_LCS2.cpp
+++ 2022/9252_LCS2.cpp
@@ -4,40 +4,50 @@
 
 using namespace std;
 
+int dp[1001][1001];
+
 int main(){
 	string str1, str2;
 	int size=0;
-	string ans = "";
 	cin>>str1>>str2;
+	for(int i=0;i<=str1.size();i++){
+		for(int j=0;j<=str2.size();j++){
+			dp[i][j]=0;
+		}
+	}
+	
 	for(int i=0;i<str1.size();i++){
 		for(int j=0;j<str2.size();j++){
-			if(str1[i] == str2[j]){
-				int tempSize = 1;
-				string tempStr = "";
-				tempStr = str1[i];
-				string tempAns = tempStr;
-				int j2Start = j+1;
-				for(int i2=i+1;i2<str1.size();i2++){
-					for(int j2=j2Start;j2<str2.size();j2++){
-						if(str1[i2]==str2[j2]){
-							tempSize++;
-							tempAns += str1[i2];
-							j2Start = j2+1;
-							break;
-						}
-					}
-				}
-				if(size<tempSize){
-					size = tempSize;
-					ans = tempAns;
-				}
+			if(str1[i]==str2[j]){
+				dp[i+1][j+1] = dp[i][j]+1;
+			}
+			else{
+				dp[i+1][j+1] = (dp[i+1][j]>dp[i][j+1])? dp[i+1][j]:dp[i][j+1];
 			}
 		}
 	}
-	cout<<size<<endl;
-	if(size!=0){
-		cout<<ans;
-	}
+	cout<<dp[str1.size()][str2.size()]<<endl;
+	int now = dp[str1.size()][str2.size()];
+	int i=str1.size();
+	int j=str2.size();
 	
+	vector<char> ans;
+	
+	while(dp[i][j]!=0){
+		if(i==0 || j == 0 ) break;
+		now = dp[i][j];
+		if(str1[i-1] == str2[j-1]){
+			ans.push_back(str1[i-1]);
+			i--;
+			j--;
+		}
+		else{
+			if(dp[i][j-1] == now) j--;
+			else if(dp[i-1][j] == now) i--;
+		}
+	}
+	for(int i=ans.size()-1;i>=0;i--){
+		cout<<ans[i];
+	}
 	return 0;
 }
